fcf0cee9514724830366d015bccd46582438e5f4
Chris Piker <cpiker@users.noreply.github.com>
2023-11-05 19:19:03 -0600

    Update windows build paths

--- buildfiles/conda/bld.bat
+++ buildfiles/conda/bld.bat
@@ -1,30 +1,24 @@
 set N_ARCH=/
 
-rem set
-rem @echo "HOWDY!"
-
-rem Not using the downloaded sources while debugging
-rem cd C:\Users\Chris\svn\das2\core\devel\libdas2
-
-nmake.exe /nologo /f makefiles\Windows.mak clean
+nmake.exe /nologo /f buildfiles\Windows.mak clean
 
 if %ERRORLEVEL% NEQ 0 (
 	EXIT /B 1
 )
 
-nmake.exe /nologo /f makefiles\Windows.mak build
+nmake.exe /nologo /f buildfiles\Windows.mak build
 
 if %ERRORLEVEL% NEQ 0 (
 	EXIT /B 2
 )
 
-nmake.exe /nologo /f makefiles\Windows.mak run_test
+nmake.exe /nologo /f buildfiles\Windows.mak run_test
 
 if %ERRORLEVEL% NEQ 0 (
 	EXIT /B 3
 )
 
-nmake.exe /nologo /f makefiles\Windows.mak install
+nmake.exe /nologo /f buildfiles\Windows.mak install
 
 if %ERRORLEVEL% NEQ 0 (
 	EXIT /B 4
